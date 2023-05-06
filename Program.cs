5a3e08fe34342b47d24819318751b5a28c7893e5
AdelHarb <lajkf@yahoo.com>
2023-05-06 16:22:36 +0300

    Repository Pattern Added With Basic Functionality

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,8 @@ builder.Services
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(connectionString)
 );
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
