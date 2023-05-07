528b18b00700bd9cc6da4674a9f795254374309c
AdelHarb <lajkf@yahoo.com>
2023-05-07 16:37:35 +0300

    Adding Categories Controller with basic Actions

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 );
 
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
